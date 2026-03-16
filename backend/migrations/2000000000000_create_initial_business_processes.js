2cf12884fb2e88c5c1b14e440049b29b4631809f
tamerlan12345 <centr-cool@mail.ru>
2026-03-16 12:37:29 +0500

    +++В

--- backend/migrations/2000000000000_create_initial_business_processes.js
+++ backend/migrations/2000000000000_create_initial_business_processes.js
@@ -2,14 +2,12 @@
 
 exports.shorthands = undefined;
 
-exports.up = (pgm) => {
-  pgm.createTable('initial_business_processes', {
-    id: { type: 'uuid', primaryKey: true, default: pgm.func('gen_random_uuid()') },
-    chat_id: { type: 'uuid', notNull: true, unique: true, references: 'chats', onDelete: 'CASCADE' },
-    content: { type: 'text', notNull: true },
-    created_at: { type: 'timestamp', default: pgm.func('current_timestamp') },
-  });
-};
+exports.up = (pgm) => pgm.createTable('initial_business_processes', {
+  id: { type: 'uuid', primaryKey: true, default: pgm.func('gen_random_uuid()') },
+  chat_id: { type: 'uuid', notNull: true, unique: true, references: 'chats', onDelete: 'CASCADE' },
+  content: { type: 'text', notNull: true },
+  created_at: { type: 'timestamp', default: pgm.func('current_timestamp') },
+}, { ifNotExists: true });
 
 exports.down = (pgm) => {
   pgm.dropTable('initial_business_processes');
